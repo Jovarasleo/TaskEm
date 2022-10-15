39e690643a3673e5f4c72e90e7fb434af6800ce4
Jovarasleo <jovarasleonavicius@gmail.com>
2022-10-15 14:32:43 +0300

    fixing dependency list, sass-loader issues and depricated modules.

--- src/views/task-manager/components/task/task.tsx
+++ src/views/task-manager/components/task/task.tsx
@@ -1,6 +1,6 @@
+import { clsx } from "clsx";
 import { useState } from "react";
 import useOutsideClick from "../../../../hooks/useOutsideClick";
-import clsx from "clsx";
 import styles from "./styles.module.scss";
 
 interface TaskProps {
