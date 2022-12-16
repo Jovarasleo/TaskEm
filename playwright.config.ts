a1b7788daba83c9e3f71aee23f9e50d1eb58e804
Jovarasleo <jovarasleonavicius@gmail.com>
2022-12-16 16:24:37 +0200

    excluded e2e test folder from jest

--- playwright.config.ts
+++ playwright.config.ts
@@ -96,8 +96,8 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
+  //   command: "npm run start",
+  //   port: 3010,
   // },
 };
 
