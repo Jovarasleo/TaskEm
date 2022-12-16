89d30a9deb6a509ee3c70bcea2ea67dd80b77ff9
Jovarasleo <jovarasleonavicius@gmail.com>
2022-12-16 16:32:40 +0200

    configuration for playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -95,10 +95,10 @@ const config: PlaywrightTestConfig = {
   // outputDir: "test-results/",
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "npm run start",
-  //   port: 3010,
-  // },
+  webServer: {
+    command: "npm run dev",
+    port: 8080,
+  },
 };
 
 export default config;
