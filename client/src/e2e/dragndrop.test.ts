b472c3bcd044d7673e4f8c3e75011bce576257f7
Jovarasleo <jovarasleonavicius@gmail.com>
2023-02-23 03:13:31 +0200

    fixed test

--- client/src/e2e/dragndrop.test.ts
+++ client/src/e2e/dragndrop.test.ts
@@ -2,9 +2,18 @@ import { test, expect } from "@playwright/test";
 
 test("drag-and-drop test", async ({ page }) => {
   await page.goto("http://localhost:8080/");
+  await page.getByRole("button").click();
+  await page.getByRole("textbox").click();
+  await page.getByRole("textbox").fill("test");
+  await page.getByRole("button", { name: "Create Project" }).click();
+  await page.getByRole("button", { name: "test" }).click();
+  // await page.getByRole("button").click();
+  await page.locator("button.menuButton ").click();
+
   await page.getByRole("button", { name: "+" }).click();
-  await page.getByRole("textbox").fill("test drag and drop");
-  await page.getByRole("textbox").press("Enter");
+  await page.locator("textarea.input").click();
+  await page.locator("textarea.input").fill("test drag and drop");
+  await page.locator("textarea.input").press("Enter");
 
   await page.dragAndDrop(
     "div >> text=test drag and drop",
